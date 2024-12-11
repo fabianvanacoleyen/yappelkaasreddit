f4a1a108372f0d5c1fa077015721487af68fc1f4
Fabian Van Acoleyen <fabianvanacoley@gmail.com>
2024-12-11 18:16:47 +0100

    bugfixes

--- app.py
+++ app.py
@@ -5,17 +5,22 @@ import json
 
 app = Flask(__name__)
 
+import requests
+
 def get_meme():
-        url = "https://meme-api.com/gimme/Yappelkaas"
-        response = json.loads(requests.request("GET", url).text)
-        meme = response.url
-        return meme
+    link = "https://meme-api.com/gimme/Yappelkaas"
+    response = requests.get(link).json()  # Haalt de JSON data op van de API
+    url = response.get('url')  # Retourneer de 'url' uit de JSON response
+    title = response.get('title')
+    print(title)
+    return url, title
+
 
 
 @app.route("/")
 def index():
-        meme = get_meme()
-        return render_template("index.html", url=meme)
+        url, title = get_meme()
+        return render_template("index.html", url=url, title=title)
 
 
 app.run(host="0.0.0.0", port=80)
